419b8854a9fd1a1553d7e25c71215994b71f7226
Eyas Ranjous <eyas.ranjous@gmail.com>
2021-10-13 00:26:02 -0500

    ?

--- src/binarySearchTreeNode.d.ts
+++ src/binarySearchTreeNode.d.ts
@@ -2,7 +2,7 @@ export class BinarySearchTreeNode<T extends number|string, U = undefined> {
   constructor(key: T, value?: U);
   setKey(key: T): BinarySearchTreeNode<T, U>;
   getKey(): T;
-  setValue(value: U): BinarySearchTreeNode<T, U>;
+  setValue(value?: U): BinarySearchTreeNode<T, U>;
   getValue(): U;
   setLeft(left?: BinarySearchTreeNode<T, U>): BinarySearchTreeNode<T, U>;
   getLeft(): BinarySearchTreeNode<T, U> | null;
