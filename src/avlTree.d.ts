d9635cd60e8d00b7a852d0a82935512a4c833f95
Eyas Ranjous <eyas.ranjous@gmail.com>
2023-01-31 04:37:51 -0800

    types

--- src/avlTree.d.ts
+++ src/avlTree.d.ts
@@ -9,9 +9,13 @@ export class AvlTree<T> extends BinarySearchTree<T> {
   max(node?: AvlTreeNode<T>): AvlTreeNode<T> | null;
   min(node?: AvlTreeNode<T>): AvlTreeNode<T> | null;
   lowerBound(value: T, includeEqual?: boolean): AvlTreeNode<T> | null;
+  lowerBoundKey(key: number|string, includeEqual?: boolean): AvlTreeNode<T> | null;
   floor(value: T, includeEqual?: boolean): AvlTreeNode<T> | null;
+  floorKey(key: number|string, includeEqual?: boolean): AvlTreeNode<T> | null;
   upperBound(value: T, includeEqual?: boolean): AvlTreeNode<T> | null;
+  upperBoundKey(key: number|string, includeEqual?: boolean): AvlTreeNode<T> | null;
   ceil(value: T, includeEqual?: boolean): AvlTreeNode<T> | null;
+  ceilKey(key: number|string, includeEqual?: boolean): AvlTreeNode<T> | null;
   root(): AvlTreeNode<T> | null;
   traverseInOrder(cb: (node: AvlTreeNode<T>) => void): void;
   traversePreOrder(cb: (node: AvlTreeNode<T>) => void): void;
